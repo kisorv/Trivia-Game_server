46c9fad5c2257caf1086d05f0e08ea2bf3713fd0
Veronica <kisorv@gmail.com>
2019-09-30 14:23:40 -0400

    I combined the backend server folder with this frontend angular version. Instead of two repos for this project, there are now one.

---
+++ routes/scores.api.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const scoresRoute = express.Router();
+const pool = require("../connection/connection");
+
+function selectAllScores(req, res) {
+  pool
+    .query("select * from scores order by score desc limit 5")
+    .then(result => {
+      res.send(result.rows);
+    });
+}
+
+scoresRoute.get("/scores", selectAllScores);
+
+scoresRoute.post("/scores", (req, res) => {
+  pool
+    .query("insert into scores (username, score) values ($1::text, $2::int)", [
+      req.body.username,
+      req.body.score
+    ])
+    .then(() => {
+      selectAllScores(req, res);
+    });
+});
+
+module.exports = scoresRoute;
